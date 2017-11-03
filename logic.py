15ea704035b5591a5039d4dd929999a3b97318e1
Carsten Bittrich <carsten.bittrich@icloud.com>
2017-11-03 11:14:57 +0100

    removed superfluous comments on task definition

--- logic.py
+++ logic.py
@@ -1,20 +1,4 @@
-#
-# CS1010FC --- Programming Methodology
-#
-# Mission N Solutions
-#
-# Note that written answers are commented out to allow us to run your
-# code easily while grading your problem set.
 from random import *
-
-#######
-#Task 1a#
-#######
-
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 1 mark for creating the correct matrix
 
 def new_game(n):
     matrix = []
@@ -23,14 +7,6 @@ def new_game(n):
         matrix.append([0] * n)
     return matrix
 
-###########
-# Task 1b #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# Must ensure that it is created on a zero entry
-# 1 mark for creating the correct loop
 
 def add_two(mat):
     a=randint(0,len(mat)-1)
@@ -40,18 +16,6 @@ def add_two(mat):
         b=randint(0,len(mat)-1)
     mat[a][b]=2 if randint(1,10)>1 else 4
     return mat
-
-###########
-# Task 1c #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# Matrix elements must be equal but not identical
-# 0 marks for completely wrong solutions
-# 1 mark for getting only one condition correct
-# 2 marks for getting two of the three conditions
-# 3 marks for correct checking
 
 def game_state(mat):
     for i in range(len(mat)):
@@ -74,16 +38,6 @@ def game_state(mat):
             return 'not over'
     return 'lose'
 
-###########
-# Task 2a #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
-
 def reverse(mat):
     new=[]
     for i in range(len(mat)):
@@ -92,16 +46,6 @@ def reverse(mat):
             new[i].append(mat[i][len(mat[0])-j-1])
     return new
 
-###########
-# Task 2b #
-###########
-
-# [Marking Scheme]
-# Points to note:
-# 0 marks for completely incorrect solutions
-# 1 mark for solutions that show general understanding
-# 2 marks for correct solutions that work for all sizes of matrices
-
 def transpose(mat):
     new=[]
     for i in range(len(mat[0])):
@@ -109,19 +53,6 @@ def transpose(mat):
         for j in range(len(mat)):
             new[i].append(mat[j][i])
     return new
-
-##########
-# Task 3 #
-##########
-
-# [Marking Scheme]
-# Points to note:
-# The way to do movement is compress -> merge -> compress again
-# Basically if they can solve one side, and use transpose and reverse correctly they should
-# be able to solve the entire thing just by flipping the matrix around
-# No idea how to grade this one at the moment. I have it pegged to 8 (which gives you like,
-# 2 per up/down/left/right?) But if you get one correct likely to get all correct so...
-# Check the down one. Reverse/transpose if ordered wrongly will give you wrong result.
 
 def cover_up(mat):
     new=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
