033a134578713bb644df7103555ec06cc64e2b61
Carsten Bittrich <carsten.bittrich@icloud.com>
2017-11-03 23:58:01 +0100

    new example algorithm: randomDirection

---
+++ algorithms/randomDirection.py
@@ -0,0 +1,11 @@
+
+import logic
+import random
+
+def getNextMoves(matrix):
+    """ alrogithm to determine which moves to do next.
+
+    return either a list of allowed moves (i.e. either 1,2,3 or 4, or as string "left", "right, "up", "down") or only the next move
+    """
+
+    return random.randint(1,4)
