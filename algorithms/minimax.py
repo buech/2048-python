3da34506fce9d26831b88225c8d2dafb3d5411c6
Adam Buechner <buechner.adam@gmx.de>
2017-11-13 19:25:19 +0100

    Make minimax work

--- algorithms/minimax.py
+++ algorithms/minimax.py
@@ -1,5 +1,6 @@
 import logic
 import numpy as np
+import random
 
 """
 The minimax algorithm.
@@ -7,62 +8,58 @@ The minimax algorithm.
 
 directions = [1,2,3,4]
 INF = 1000000000
-MAX_DEPTH = 2
 
-nextMove = 0
-
-def generateValidMoves(player, grid):
+def generateValidMoves(grid):
     validMoves = []
-    if player == 1:
-        for d in directions:
-            if logic.direction(grid, d)[1]:
-                validMoves.append(d)
-            else:
-                pass
+    for i in range(4):
+        for j in range(4):
+            if grid[i][j] == 0:
+                validMoves.append([i,j])
 
-        return validMoves
-
-    else:
-        for i in range(4):
-            for j in range(4):
-                if grid[i][j] == 0:
-                    validMoves.append([i,j])
-
-        return validMoves
+    return validMoves
 
 def addTile(grid, i, j, num):
     grid[i][j] = num
     return grid
 
-def search_max(player, depth, grid):
-    validMoves = generateValidMoves(player, grid)
-    if (depth == 0) or (len(validMoves) == 0):
-        return evaluate(grid)
+def search_max(grid, depth):
     maxScore = -INF
-    for move in validMoves:
-        new_grid, done = logic.direction(grid, move)
-        score = search_min(-player, depth-1, new_grid)
+
+    if depth == 0:
+        return evaluate(grid)
+
+    i = 0
+    for move in directions:
+        new_grid, moved = logic.direction(grid, move)
+        if not moved:
+            i += 1
+            continue
+        score = search_min(grid, depth-1)
         if score > maxScore:
             maxScore = score
-            if depth == MAX_DEPTH:
-                nextMove = move
+
+    if i > 3:
+        return evaluate(grid)
 
     return maxScore
 
-def search_min(player, depth, grid):
-    validMoves = generateValidMoves(player, grid)
+def search_min(grid, depth):
+    validMoves = generateValidMoves(grid)
+
     if (depth == 0) or (len(validMoves) == 0):
         return evaluate(grid)
+
     minScore = INF
+
     for i,j in validMoves:
         for num in [2,4]:
             new_grid = addTile(grid, i, j, num)
-            score = search_max(-player, depth-1, new_grid)
+            score = search_max(new_grid, depth-1)
             if score < minScore:
                 minScore = score
 
     return minScore
-    
+
 def evaluate(grid):
     return logic.score(grid)
 
@@ -71,11 +68,22 @@ def getNextMoves(matrix):
 
     return either a list of allowed moves (i.e. either 1,2,3 or 4, or as string "left", "right, "up", "down") or only the next move
     """
-    global nextMove
 
-    depth = MAX_DEPTH
-    score = search_max(1, depth, matrix)
+    maxDepth = 3
+    maxScore = -INF
+    best_move = random.choice(directions)
 
-    print score, nextMove
+    for move in directions:
+        grid, moved = logic.direction(matrix, move)
+        if not moved:
+            continue
 
-    return nextMove
+        score = search_min(grid, maxDepth)
+
+        if score > maxScore:
+            maxScore = score
+            best_move = move
+
+    print score, best_move
+
+    return best_move
