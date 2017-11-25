ce447250053358e628d436b2ab6ba6791e55f2c2
Adam Buechner <buechner.adam@gmx.de>
2017-11-25 11:52:40 +0100

    Profile my algorithms

--- profile.py
+++ profile.py
@@ -1,8 +1,8 @@
 import cProfile
-import algorithms.minimax as mm
-import algorithms.expecti as ex
+from algorithms import minimax, alpha_beta, expecti
 
 grid = [[0,0,2,4],[0,2,8,4],[2,2,8,32],[16,16,64,128]]
 
-cProfile.run('ex.getNextMoves(grid)')
-cProfile.run('mm.getNextMoves(grid)')
+cProfile.run('minimax.getNextMoves(grid)')
+cProfile.run('alpha_beta.getNextMoves(grid)')
+cProfile.run('expecti.getNextMoves(grid)')
