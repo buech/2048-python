9f77a2e1d0e3fe2607c3a96ffb4ce44b5253a347
Adam Buechner <buechner.adam@gmx.de>
2017-11-25 18:06:06 +0100

    Sort profiler stats by cumtime

--- profile.py
+++ profile.py
@@ -3,6 +3,8 @@ from algorithms import minimax, alpha_beta, expecti
 
 grid = [[0,0,2,4],[0,2,8,4],[2,2,8,32],[16,16,64,128]]
 
-cProfile.run('minimax.getNextMoves(grid)')
-cProfile.run('alpha_beta.getNextMoves(grid)')
-cProfile.run('expecti.getNextMoves(grid)')
+sort_key = 'cumtime'
+
+cProfile.run('minimax.getNextMoves(grid)', sort=sort_key)
+cProfile.run('alpha_beta.getNextMoves(grid)', sort=sort_key)
+cProfile.run('expecti.getNextMoves(grid)', sort=sort_key)
