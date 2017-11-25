b055362d2b19fe76257f429944acffc2f16be4cc
Adam Buechner <buechner.adam@gmx.de>
2017-11-25 10:38:17 +0100

    Also profile expecti

--- profile.py
+++ profile.py
@@ -1,6 +1,8 @@
 import cProfile
 import algorithms.minimax as mm
+import algorithms.expecti as ex
 
 grid = [[0,0,2,4],[0,2,8,4],[2,2,8,32],[16,16,64,128]]
 
+cProfile.run('ex.getNextMoves(grid)')
 cProfile.run('mm.getNextMoves(grid)')
