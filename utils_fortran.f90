5440681d2a9afe2f60998a0aac5b2841d2bcdf63
Adam Buechner <buechner.adam@gmx.de>
2017-12-02 10:42:11 +0100

    Implement expectiminimax in fortran

--- utils_fortran.f90
+++ utils_fortran.f90
@@ -1,5 +1,6 @@
 module utils
    contains
+
       function shift_left(grid) result(merged)
          implicit none
          integer, dimension(4,4), intent(in) :: grid
@@ -93,13 +94,133 @@ module utils
 
 end module utils
 
+module eval
+   contains
+
+      function count_free_tiles(grid) result(n)
+         implicit none
+         integer, dimension(4,4), intent(in) :: grid
+         integer :: i, j, n
+
+         n = 0
+         do i=1,4
+            do j=1,4
+               if(grid(i,j) == 0) then
+                  n = n + 1
+               end if
+            end do
+         end do
+
+      end function
+
+      function evaluate(grid) result(score)
+         implicit none
+         integer, dimension(4,4), intent(in) :: grid
+         integer :: score
+
+         score = count_free_tiles(grid)
+
+      end function
+
+end module eval
+
+module expecti
+   use utils
+   use eval
+
+   integer, parameter :: inf = 1E08
+   integer, dimension(2) :: num = (/2, 4/)
+   real, dimension(2) :: p = (/0.9, 0.1/)
+
+   contains
+
+      function search_max(grid, depth) result(max_score)
+         implicit none
+         integer, dimension(4,4), intent(in) :: grid
+         integer, intent(in) :: depth
+
+         integer, dimension(4,4) :: new_grid
+         integer :: move, score, max_score
+
+         max_score = -inf
+         do move=1,4
+            new_grid = direction(grid, move)
+            if(all(new_grid == grid)) then
+               cycle
+            end if
+            score = search_min(new_grid, depth-1)
+            if(score > max_score) then
+               max_score = score
+            end if
+         end do
+
+      end function search_max
+
+      function search_min(grid, depth) result(score)
+         implicit none
+         integer, dimension(4,4) :: grid
+         integer, intent(in) :: depth
+         integer :: n, i, j, free, score
+
+         if(depth == 0) then
+            score = evaluate(grid)
+            return
+         end if
+
+         score = 0
+         free = 0
+         do n=1,2
+            do i=1,4
+               do j=1,4
+                  if(grid(i,j) == 0) then
+                     grid(i,j) = num(n)
+                     free = free + 1
+                     score = score + p(n) * search_max(grid, depth)
+                     grid(i,j) = 0
+                  end if
+               end do
+            end do
+            if(free == 0) then
+               score = evaluate(grid)
+               return
+            end if
+         end do
+
+         score = 2 * score / free
+
+      end function
+
+      function get_next_move(grid) result(best_move)
+         implicit none
+         integer, dimension(4,4), intent(in) :: grid
+         integer, dimension(4,4) :: new_grid
+         integer :: max_score = -inf
+         integer :: score, move, best_move
+
+         best_move = 1
+         do move = 1,4
+            new_grid = direction(grid, move)
+            if(all(new_grid == grid)) then
+               cycle
+            end if
+            score = search_min(new_grid, 1)
+            if(score > max_score) then
+               max_score = score
+               best_move = move
+            end if
+         end do
+
+      end function
+
+end module expecti
+
 program test
    use utils
+   use expecti
 
    implicit none
 
    integer, dimension(4,4) :: grid, merged
-   integer :: i,j
 
    grid = reshape((/0, 0, 2, 2,&
                     0, 2, 0, 4,&
@@ -133,5 +254,7 @@ program test
    print *, 'v'
    print '(4 i3)', merged(:,:)
    print *
+
+   print *, get_next_move(grid)
 
 end program test
