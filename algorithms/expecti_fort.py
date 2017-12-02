14caeaeaf169685f4f7ff20310ef854297f6e5bf
Adam Buechner <buechner.adam@gmx.de>
2017-12-02 11:37:08 +0100

    Add algorithm using fortran expectiminimax

---
+++ algorithms/expecti_fort.py
@@ -0,0 +1,19 @@
+import utils_fortran
+import numpy as np
+import random
+
+"""
+The minimax algorithm.
+"""
+
+def getNextMoves(matrix):
+    """ alrogithm to determine which moves to do next.
+
+    return either a list of allowed moves (i.e. either 1,2,3 or 4, or as string "left", "right, "up", "down") or only the next move
+    """
+
+    max_depth = 1
+
+    best_move = utils_fortran.expecti.get_next_move(np.array(matrix).T, max_depth)
+
+    return best_move if best_move else random.choice((1,2,3,4))
