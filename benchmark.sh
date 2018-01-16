387e7c6b9cc2b4f5701d2e90d961ca361d92744e
Adam Buechner <buechner.adam@gmx.de>
2018-01-16 19:24:48 +0100

    Improve benchamrk script

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
 
-parallel -n0 'python runner.py -a expecti_fort -s$RANDOM' ::: {1..4}
+NGAMES=8
+NTHREADS=2
+NLOOPS=$(expr $NGAMES / $NTHREADS)
+
+if grep -P > /dev/null 2>&1; then
+	GREP='grep -P'
+else
+	GREP='pcregrep'
+fi
+
+echo "Running $(expr $NLOOPS \* $NTHREADS) games in $NTHREADS threads, behold!"
+
+for i in $(seq 1 $NLOOPS); do
+	parallel -n0 'python runner.py -a expecti_fort -s$RANDOM' ::: $(seq 1 $NTHREADS) | $GREP '^(?!(Algorithm|\-\-))'
+done
