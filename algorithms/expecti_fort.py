dcf5a745dfcc03f1bee3b88f5a4714ea9ca19465
Adam Buechner <buechner.adam@gmx.de>
2018-01-18 17:08:37 +0100

    Convert grid to logspace in python

--- algorithms/expecti_fort.py
+++ algorithms/expecti_fort.py
@@ -14,6 +14,9 @@ def getNextMoves(matrix):
 
     max_depth = 2
 
-    best_move = utils_fortran.expecti.get_next_move(np.array(matrix), max_depth)
+    mat_np = np.array(matrix)
+    mat_log = np.log2(mat_np, where=mat_np!=0)
+
+    best_move = utils_fortran.expecti.get_next_move(mat_log, max_depth)
 
     return best_move if best_move else random.choice((1,2,3,4))
