9c53260c1bbfa99d11343a84dd419ad9480e7ea0
Adam Buechner <buechner.adam@gmx.de>
2018-01-29 17:51:32 +0100

    Move needed routines from bitutils to algorithm

--- algorithms/expecti_c.py
+++ algorithms/expecti_c.py
@@ -1,21 +1,24 @@
 import random
 import ctypes
-import bitutils
 from math import log
 import os
 
-"""
-The minimax algorithm.
-"""
 
-lib = ctypes.CDLL(os.getcwd() + '/utils.so')
+solver = ctypes.CDLL(os.getcwd() + '/utils.so')
 
-#print 'Initializing tables ...'
-lib.init()
-#print 'Done.'
+solver.init()
 
-lib.get_next_move.argtypes = [ctypes.c_uint64, ctypes.c_int]
-lib.get_next_move.restype = ctypes.c_int
+solver.get_next_move.argtypes = [ctypes.c_uint64, ctypes.c_int]
+solver.get_next_move.restype = ctypes.c_int
+
+def encode_row(row):
+    return row[0] << 12 | row[1] << 8 | row[2] << 4 | row[3]
+
+def encode(board):
+    return (encode_row(board[0]) << 48 |
+            encode_row(board[1]) << 32 |
+            encode_row(board[2]) << 16 |
+            encode_row(board[3]))
 
 def unique(a):
     u = []
@@ -23,17 +26,12 @@ def unique(a):
     return u
 
 def getNextMoves(matrix):
-    """ alrogithm to determine which moves to do next.
-
-    return either a list of allowed moves (i.e. either 1,2,3 or 4, or as string "left", "right, "up", "down") or only the next move
-    """
-
     max_depth = max(3, len(unique(matrix)) - 2)
 
     matrix = [[0 if x==0 else int(log(x,2)) for x in r] for r in matrix]
 
-    board = bitutils.encode(matrix)
+    board = encode(matrix)
 
-    best_move = lib.get_next_move(board, max_depth)
+    best_move = solver.get_next_move(board, max_depth)
 
     return best_move if best_move else random.choice((1,2,3,4))
