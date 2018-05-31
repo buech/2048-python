ea709e41056e18a9dcd8afa95c98d1ef084d38a8
Adam Buechner <buechner.adam@gmx.de>
2018-05-31 18:03:09 +0200

    Use plain grep

--- benchmark.sh
+++ benchmark.sh
@@ -3,12 +3,6 @@
 NGAMES=8
 NTHREADS=2
 
-if grep -P --help > /dev/null 2>&1; then
-	GREP='grep -P'
-else
-	GREP='pcregrep'
-fi
-
 echo "Running $NGAMES games in $NTHREADS threads, behold!"
 
-parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | $GREP '^(?!(Algorithm|\-\-))'
+parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
