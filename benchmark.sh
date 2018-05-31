4a59710947a61032fa4f5d39c2900d013dd9d51c
Adam Buechner <buechner.adam@gmx.de>
2018-05-31 17:42:52 +0200

    Fix number of samples in benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -11,4 +11,4 @@ fi
 
 echo "Running $NGAMES games in $NTHREADS threads, behold!"
 
-parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NTHREADS) | $GREP '^(?!(Algorithm|\-\-))'
+parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | $GREP '^(?!(Algorithm|\-\-))'
