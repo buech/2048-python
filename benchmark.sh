eeeb8dc040a17ca2eeb971be0fd1d4508df959d7
Adam Buechner <buechner.adam@gmx.de>
2018-06-01 15:47:22 +0200

    Switch back to grep

--- benchmark.sh
+++ benchmark.sh
@@ -5,5 +5,5 @@ NTHREADS=2
 
 echo "Running $NGAMES games in $NTHREADS threads, behold!"
 
-#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
-parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
+parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
+#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
