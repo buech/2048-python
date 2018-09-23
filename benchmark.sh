d9143d9a90807acc2ddf3064b6dbd54b769f1ac4
Adam Buechner <buechner.adam@gmx.de>
2018-09-23 14:58:18 +0200

    xargs version of benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -5,5 +5,7 @@ NTHREADS=2
 
 echo "Running $NGAMES games in $NTHREADS threads"
 
-parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
-#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$(od -An -N2 -tu2 /dev/urandom)' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
+for i in $(seq 1 $NGAMES); do echo $RANDOM; done | xargs -I% -P$NTHREADS python runner.py -a $1 -s% | grep -v '^\(Algorithm\|\-\+\)'
+#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
+#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
+# alternative to $RANDOM : $(od -An -N2 -tu2 /dev/urandom)
