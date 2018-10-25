7a0d520c2c5e1116b7143ca9fc9f2845068cd29a
Adam Buechner <buechner.adam@gmx.de>
2018-10-25 20:37:01 +0200

    Use printf instead of echo

--- benchmark.sh
+++ benchmark.sh
@@ -3,9 +3,9 @@
 NGAMES=8
 NTHREADS=2
 
-echo "Running $NGAMES games in $NTHREADS threads"
+printf "Running $NGAMES games in $NTHREADS threads\n"
 
-for i in $(seq 1 $NGAMES); do echo $RANDOM; done | xargs -I% -P$NTHREADS python runner.py -a $1 -s% | grep -v '^\(Algorithm\|\-\+\)'
+for i in $(seq 1 $NGAMES); do printf "$RANDOM\n"; done | xargs -I% -P$NTHREADS python runner.py -a $1 -s% | grep -v '^\(Algorithm\|\-\+\)'
 #parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
 #parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
 # alternative to $RANDOM : $(od -An -N2 -tu2 /dev/urandom)
