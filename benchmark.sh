c830ae3d2ebeac9ad58113f834eeceed9dd287b8
Adam Buechner <buechner.adam@gmx.de>
2018-10-28 15:03:46 +0100

    Clean up benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,3 @@ NTHREADS=2
 printf "Running $NGAMES games in $NTHREADS threads\n"
 
 for i in $(seq 1 $NGAMES); do printf "$RANDOM\n"; done | xargs -I% -P$NTHREADS python runner.py -a $1 -s% | grep -v '^\(Algorithm\|\-\+\)'
-#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | grep -v '^\(Algorithm\|\-\+\)'
-#parallel -j$NTHREADS -n0 python runner.py -a $1 -s'$RANDOM' ::: $(seq 1 $NGAMES) | awk '!/^(Algorithm|-+)/' -
-# alternative to $RANDOM : $(od -An -N2 -tu2 /dev/urandom)
