0ff9261f919702664ed36ac365d02fd2a477c59c
Adam Buechner <buechner.adam@gmx.de>
2019-10-17 17:55:32 +0300

    Use python3

--- benchmark.sh
+++ benchmark.sh
@@ -5,4 +5,4 @@ NTHREADS=${NTHREADS:-2}
 
 printf "Running $NGAMES games in $NTHREADS threads\n"
 
-seq $NGAMES | xargs -I% -P$NTHREADS sh -c 'python runner.py -a '$1' -s$(od -An -N2 -tu2 /dev/random)' | grep -v '^\(Algorithm\|\-\+\)'
+seq $NGAMES | xargs -I% -P$NTHREADS sh -c 'python3 runner.py -a '$1' -s$(od -An -N2 -tu2 /dev/random)' | grep -v '^\(Algorithm\|\-\+\)'
